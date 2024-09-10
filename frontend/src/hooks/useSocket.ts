cd18a200d2d6f88e4abb732cae712691dab8bf7e
hp113 <hariprasad2315@gmail.com>
2024-09-10 23:59:54 +0530

    change to frontend websocket server address

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const WS_URL = "chessbackend-ald9dj5y7-hariprasads-projects-eac4bd11.vercel.app"
+const WS_URL = "wss://chessbackend-production.up.railway.app/"
 export const useSocket = () =>{
     const  [socket, setSocket] = useState<WebSocket | null>(null);
 
